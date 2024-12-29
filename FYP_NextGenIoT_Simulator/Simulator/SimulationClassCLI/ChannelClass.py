0495bb6195ac0a793d1bedf3beb93b9ba28913c6
The_Steel_Sentinel <rubythihan@gmail.com>
2024-12-29 17:15:04 +0800

    Update ChannelClass.py

--- FYP_NextGenIoT_Simulator/Simulator/SimulationClassCLI/ChannelClass.py
+++ FYP_NextGenIoT_Simulator/Simulator/SimulationClassCLI/ChannelClass.py
@@ -1,4 +1,4 @@
-from numpy import sum, abs, sqrt, arange, sinc, hamming, convolve
+from numpy import sum, abs, sqrt, arange, sinc, hamming, convolve, exp, pi
 from numpy.random import normal
 
 class SimpleGWNChannel_dB:
@@ -30,4 +30,48 @@ class SimpleDelayChannel:
         h *= hamming(N) # window the filter to make sure it decays to 0 on both sides
         h /= sum(h) # normalize to get unity gain, we don't want to change the amplitude/power
         signal = convolve(signal, h) # apply filter
+        return signal
+    
+class SimpleFlatFadingChannel:
+    def __init__(self, type):
+        if type != "rayleigh" and type != "rician":
+            raise ValueError("Invalid type, must be 'rayleigh' or 'rician'")
+        elif type == 'rician':
+            rician_k = int(input("Enter the Rician K factor: "))
+        self.type = type
+        
+    def add_fading(self, signal):
+        
+        if self.type == "rayleigh":
+            # Diffuse component (Rayleigh fading)
+            h = (normal(0, 1, size=signal.shape) + 1j * normal(0, 1, size=signal.shape)) / sqrt(2)
+                
+        elif self.type == "rician":
+            # Line-of-sight (LOS) component
+            los_component = sqrt(self.rician_k / (self.rician_k + 1))
+            
+            # Diffuse component (Rayleigh fading)
+            diffuse_component = (normal(0, 1, size=signal.shape) + 1j * normal(0, 1, size=signal.shape)) / sqrt(2)
+            
+            # Rician fading: LOS + diffuse components
+            h = los_component + sqrt(1 / (self.rician_k + 1)) * diffuse_component
+            
+        return signal * h
+
+class SimpleFrequencyDriftChannel:
+    def __init__(self, frequency_drift_rate):
+        self.frequency_drift_rate = frequency_drift_rate
+        
+    def add_drift(self, signal):
+        # Apply frequency drift to the signal
+        signal = signal * exp(1j * 2 * pi * self.frequency_drift_rate * arange(len(signal)) / len(signal))
+        return signal
+
+class SimpleFrequencyOffsetChannel:
+    def __init__(self, frequency_offset):
+        self.frequency_offset = frequency_offset
+        
+    def add_offset(self, signal):
+        # Apply frequency offset to the signal
+        signal = signal * exp(1j * 2 * pi * self.frequency_offset)
         return signal
