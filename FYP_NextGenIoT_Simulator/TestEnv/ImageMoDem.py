ad689c11cc7a4b247fc28fe0f567d8219964158b
The_Steel_Sentinel <rubythihan@gmail.com>
2024-12-29 17:15:13 +0800

    Create ImageMoDem.py

---
+++ FYP_NextGenIoT_Simulator/TestEnv/ImageMoDem.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+import sys
+
+sys.path.insert(1, r"FYP_NextGenIoT_Simulator\Simulator")
+from SimulationClassCompact.ModulationClass import Modulator
+from SimulationClassCompact.DemodulationClass import Demodulator
+
+out = io.BytesIO()
+
+with Image.open(rf"FYP_NextGenIoT_Simulator\SNR_BER.png") as img:
+    img.save(out, format="png")
+
+image_in_bytes = out.getvalue()
+
+encoded_b2 = "".join([format(n, '08b') for n in image_in_bytes])
+
+modulation_mode = input("Enter modulation mode (BPSK, QPSK, QAM 16/64/256/1024/4096): ").upper()
+bit_rate = int(input("Enter bit rate: "))
+carrier_frequency = int(input("Enter carrier frequency: "))
+
+modulator = Modulator(modulation_mode, bit_rate, carrier_frequency)
+demodulator = Demodulator(modulation_mode, bit_rate, carrier_frequency)
+
+bitstr = list(encoded_b2)
+print(len(bitstr)/8)
+'''if len(bitstr) % modulator.order != 0:
+    bitstr.extend(['0']*(modulator.order - len(bitstr) % modulator.order))
+bitstr.extend(['0', '0'])
+
+bitstream = bitstr
+
+_, modulated_signal = modulator.modulate(bitstream)
+
+demodulated_signal = demodulator.demodulate(modulated_signal)
+
+_, bitarray = demodulator.demapping(demodulated_signal)
+
+decoded_b2 = [int(bitarray[i:i + 8], 2) for i in range(0, len(bitarray), 8)]
+
+with open(rf"FYP_NextGenIoT_Simulator\SNR_BER_decoded.png", 'wb') as f:
+    f.write(bytes(decoded_b2))'''
