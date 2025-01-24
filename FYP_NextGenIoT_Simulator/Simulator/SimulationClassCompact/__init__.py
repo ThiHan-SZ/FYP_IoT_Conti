ddf8538efe512db25ef40416dde0375d5b9dc0f4
The_Steel_Sentinel <rubythihan@gmail.com>
2025-01-25 02:08:06 +0800

    Update __init__.py

---
+++ FYP_NextGenIoT_Simulator/Simulator/SimulationClassCompact/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package contains the simulation classes for the NextGen IoT Simulator.
+
+Classes are stored in the following modules:
+- ModulationClass: Contains the Modulator class for modulation simulation.
+- DemodulationClass: Contains the Demodulator class for demodulation simulation.
+- ChannelClass: Contains the Channel class for channel simulation.
+
+Classes are non CLI and ready for Import
+"""
