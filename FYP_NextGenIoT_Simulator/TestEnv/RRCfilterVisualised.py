3f28e403154aac5a7b18c3adea4b24920422332f
The_Steel_Sentinel <rubythihan@gmail.com>
2025-01-30 17:11:50 +0800

    Update RRCfilterVisualised.py

--- FYP_NextGenIoT_Simulator/TestEnv/RRCfilterVisualised.py
+++ FYP_NextGenIoT_Simulator/TestEnv/RRCfilterVisualised.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import commpy
 
-Ts = 1/2000
-Fs = 200000
+Ts = 8/16000
+Fs = 4e6
 RRC_alpha = 0.35
 
 samples_per_symbol = int(Ts * Fs)
 RRC_delay = 3 * Ts
 
-def RRCfilter(N, alpha, Ts, Fs):
+def RRCFilter(N, alpha, Ts, Fs):
     """
     Generates a root raised cosine (RRC) filter (FIR) impulse response. Adapted from commpy due to small bug, filter does not extend to full range of samples.
 
@@ -61,53 +61,16 @@ def RRCfilter(N, alpha, Ts, Fs):
 
 
 
-t, rrc1 = myRRCfilter(
-            N=int(2*Fs*RRC_delay),
-            alpha=0.1,
-            Ts=Ts, 
-            Fs=Fs
-        )
-t, rrc2 = myRRCfilter(
-            N=int(2*Fs*RRC_delay),
-            alpha=0.25,
-            Ts=Ts, 
-            Fs=Fs
-        )
-t, rrc3 = myRRCfilter(
-            N=int(2*Fs*RRC_delay),
-            alpha=0.5,
-            Ts=Ts, 
-            Fs=Fs
-        )
-t, rrc4 = myRRCfilter(
-            N=int(2*Fs*RRC_delay),
-            alpha=1,
-            Ts=Ts, 
-            Fs=Fs
-        )
+t, rrc1 = RRCFilter(N = int(2*Fs*RRC_delay), alpha = 0, Ts = Ts, Fs = Fs)
+plt.plot(t, rrc1)
+plt.plot(t,rrc1**2)
 
-t,rrc5 = myRRCfilter(
-            N=int(2*Fs*RRC_delay),
-            alpha=0.1,
-            Ts=Ts, 
-            Fs=Fs
-        )
-
-fig,(ax1,ax2) = plt.subplots(2,1)
-ax1.plot(t, rrc1, label = 'alpha = 0.1')
-ax1.plot(t, rrc2, label = 'alpha = 0.25')
-ax1.plot(t, rrc3, label = 'alpha = 0.5')
-ax1.plot(t, rrc4, label = 'alpha = 1')
-ax2.plot(t,rrc1**2, label = '0.1 RC')
-ax2.plot(t,rrc2**2, label = '0.25 RC')
-ax2.plot(t,rrc3**2, label = '0.5 RC')
-ax2.plot(t,rrc4**2, label = '1 RC')
-ax1.plot(t,[0]*len(t), 'k--')
-ax1.axvline(x = 0, color = 'b', label = '0 Line')
-ax2.plot(t,[0]*len(t), 'k--')
-ax2.axvline(x = 0, color = 'b', label = '0 Line')
+plt.plot(t,[0]*len(t), 'k--')
+plt.axvline(x = 0, color = 'b', label = '0 Line')
+#ax2.plot(t,[0]*len(t), 'k--')
+#ax2.axvline(x = 0, color = 'b', label = '0 Line')
 for i in np.arange(-3,4)*Ts:
-    ax1.axvline(i, -0.1, 0.9,linestyle = 'dashed')
-    ax2.axvline(i, -0.1, 0.9,linestyle = 'dashed')
+    plt.axvline(i, -0.1, 0.9,linestyle = 'dashed')
+    #ax2.axvline(i, -0.1, 0.9,linestyle = 'dashed')
 plt.legend()
 plt.show()
