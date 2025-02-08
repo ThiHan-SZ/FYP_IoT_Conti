86e230075f4ee4a88685a41e331f956d0d505080
wykgit1 <yankai200507@gmail.com>
2025-02-09 01:14:47 +0800

    Update ModulationClass.py

--- FYP_NextGenIoT_Simulator/Simulator/SimulationClassCompact/ModulationClass.py
+++ FYP_NextGenIoT_Simulator/Simulator/SimulationClassCompact/ModulationClass.py
@@ -23,6 +23,7 @@ class Modulator:
         self.order = self.modulation_modes[modulation_mode]
 
         #Bit Rate Parameters
+        #pre bit to baud uncorrupted
         self.baud_rate = bit_rate/self.order
         self.symbol_period = 1/self.baud_rate
 
